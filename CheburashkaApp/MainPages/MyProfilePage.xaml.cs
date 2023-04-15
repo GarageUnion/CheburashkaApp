5dd9c957bfa870555edf7879226379d3daa5d2ec
PK <PK@DESKTOP-10QCO5S>
2023-04-15 11:00:15 +0300

    Added exit button

--- CheburashkaApp/MainPages/MyProfilePage.xaml.cs
+++ CheburashkaApp/MainPages/MyProfilePage.xaml.cs
@@ -6,4 +6,11 @@ public partial class MyProfilePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void LogoutButton_Clicked(object sender, EventArgs e)
+    {
+		var userIO = new UserDataIO();
+		userIO.DeleteFile();
+        Navigation.PushAsync(new LoginPage());
+    }
 }
